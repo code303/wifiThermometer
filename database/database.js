688ec42f5cf609e5638cb7151c07a8fef9ba0fff
code303 <ttthhh@gmx.de>
2023-06-21 11:59:50 +0200

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,17 +1,43 @@
+const fs = require('fs');
+const FILE = './data/db.csv';
+
 const database = {
     
 };
 
-const getSamples = () => {
+const readSamples = () => {
     return [];
 };
 
-const getSample = (id) => {
+const readSample = (id) => {
     return {
         id: id,
         timestamp:  new Date().toISOString(),
-        value: 12.3};
+        temperature: 12.3,
+        humidity: 59.6
+    };
+};
+
+const writeSample = (sample) => {
+    console.log(JSON.stringify(sample));
+    const entry =
+      sample.timestamp.toISOString() + ';' +
+      sample.temperature + ';' +
+      sample.humidity;
+
+    writeToFile(entry);
 };
 
 
-module.exports = { getSamples, getSample };
+
+const writeToFile = (content) => {
+    fs.appendFile(FILE, content, err => {
+        if (err) {
+            console.error(err);
+        }
+        console.log('file written successfully');
+    });
+};
+
+
+module.exports = { readSamples, readSample, writeSample };
