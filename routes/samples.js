e747539275d0336f2b4c274f94390a00e2882d70
code303 <mail@example.com>
2023-06-23 01:33:40 +0200

    Add POST for create a new sample

--- routes/samples.js
+++ routes/samples.js
@@ -19,14 +19,31 @@ router.post('/body', (req, res) => {
     const sample = {
         id: linuxTimestamp,
         timestamp: isoDateString,
-        temperature: req.query.temperature,
-        humidity: req.query.humidity
+        temperature: req.body.temperature,
+        humidity: req.body.humidity
     };
         
     database.writeSample(sample);
     res.statusCode = 201;
     res.json(sample);
 });
+
+router.post('/bodyjson', (req, res) => {
+    console.log('POST');
+    const linuxTimestamp = Date.now();
+    const isoDateString = new Date(linuxTimestamp).toISOString();
+    const sample = {
+        id: linuxTimestamp,
+        timestamp: isoDateString,
+        temperature: req.body.temperature,
+        humidity: req.body.humidity
+    };
+        
+    database.writeSample(sample);
+    res.statusCode = 201;
+    res.json(sample);
+});
+
 
 router.post('/query', (req, res) => {
     console.log('POST');
